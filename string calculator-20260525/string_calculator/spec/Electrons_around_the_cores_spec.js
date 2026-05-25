af8fb49c4e81adfb1c7858390bce3060b2644022
Muhittin <muhittinsungur40@gmail.com>
2026-05-25 15:01:39 +0200

    055 changed describtion of spec file to match the exercise also refactoring

--- string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
+++ string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
@@ -1,9 +1,9 @@
-describe('String Calculator', function() {
+describe('Electrons around the cores', function() {
 	beforeEach(function() {
 		calculator = new StringCalculator();
 	});
 
-	it('should return 0 for an empty string', function() {
+	it('should return 0 for an empty dice array', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
 });
