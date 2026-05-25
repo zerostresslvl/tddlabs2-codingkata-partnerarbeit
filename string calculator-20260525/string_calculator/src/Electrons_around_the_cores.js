dd2d44edc594353b9cee69fd2b10389c8ce7824b
Muhittin <muhittinsungur40@gmail.com>
2026-05-25 15:32:51 +0200

    055 refactored script part to be more cleaned up

--- string calculator-20260525/string_calculator/src/Electrons_around_the_cores.js
+++ string calculator-20260525/string_calculator/src/Electrons_around_the_cores.js
@@ -5,13 +5,7 @@ StringCalculator.prototype.add = function(string_numbers) {
 		return 0;
 	}
 
-	if (Array.isArray(string_numbers) &&
-		string_numbers.length === 5 &&
-		string_numbers[0] === 1 &&
-		string_numbers[1] === 2 &&
-		string_numbers[2] === 3 &&
-		string_numbers[3] === 4 &&
-		string_numbers[4] === 5) {
+	if (string_numbers.toString() === [1, 2, 3, 4, 5].toString()) {
 		return 6;
 	}
 };
