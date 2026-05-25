05e2e1ddb576e7c02cbd9b3c4fabb7be042c6e62
zerostresslvl <zerostresslvl@gmail.com>
2026-05-25 13:31:09 +0200

    initial commit

---
+++ string calculator-20260525/string_calculator/spec/string_calculator_spec.js
@@ -0,0 +1,9 @@
+describe('String Calculator', function() {
+	beforeEach(function() {
+		calculator = new StringCalculator();
+	});
+
+	it('should return 0 for an empty string', function() {
+		expect(calculator.add('')).toEqual(0);
+	});
+});
---
+++ string calculator-20260525/string_calculator/src/string_calculator.js
@@ -0,0 +1,5 @@
+// StringCalculator.js
+function StringCalculator() {}
+StringCalculator.prototype.add = function(string_numbers) {
+	//
+};
