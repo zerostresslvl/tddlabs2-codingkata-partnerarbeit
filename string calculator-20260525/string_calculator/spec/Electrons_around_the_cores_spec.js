ce0a04e66502f3bafff36b97f80013546ea41786
zerostresslvl <zerostresslvl@gmail.com>
2026-05-25 15:52:45 +0200

    added test case for the last case now

--- string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
+++ string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
@@ -18,4 +18,8 @@ describe('Electrons around the cores', function() {
 	it('should return the correct result for [6, 6, 4, 4, 1, 3]', function() {
 		expect(calculator.add([6, 6, 4, 4, 1, 3])).toEqual(2);
 	});
+
+	it('should return the correct result for [3, 5, 3, 5, 4, 2]', function() {
+		expect(calculator.add([3, 5, 3, 5, 4, 2])).toEqual(12);
+	});
 });
