71e710305369b2cdbf897c5b7196a204c36964a1
Muhittin <muhittinsungur40@gmail.com>
2026-05-25 15:20:13 +0200

    055 added failing test for returning correct result of [1, 2, 3, 4, 5]

--- string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
+++ string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
@@ -6,4 +6,8 @@ describe('Electrons around the cores', function() {
 	it('should return 0 for an empty dice array', function() {
 		expect(calculator.add('')).toEqual(0);
 	});
+
+	it('should return the correct result for [1, 2, 3, 4, 5]', function() {
+	expect(calculator.add([1, 2, 3, 4, 5])).toEqual(6);
+});
 });
