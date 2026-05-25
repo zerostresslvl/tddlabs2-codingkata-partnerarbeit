057c14cdb3082e576c7bfdca20cbbb5ecad6a74f
zerostresslvl <zerostresslvl@gmail.com>
2026-05-25 16:16:00 +0200

    found a generic solution  for all of it - added an edge case also so we can test more numbers -> ensure the numbers within the array are between 1 and 6 and as for the actual testing itself we grab a number -> check if it's odd or even -> if it's odd with 1 rest we subtract 1 from the number for example 1, 3, 5 become 0, 2, 4 and we return that as our total -> all the numbers are then added up and added as our arrays total -> even numbers are ignored anyway so they're not relevant

--- string calculator-20260525/string_calculator/src/Electrons_around_the_cores.js
+++ string calculator-20260525/string_calculator/src/Electrons_around_the_cores.js
@@ -1,26 +1,21 @@
 // StringCalculator.js
 function StringCalculator() {}
-function matchesNumbers(actual_numbers, expected_numbers) {
-	return actual_numbers.toString() === expected_numbers.toString();
-}
 StringCalculator.prototype.add = function(string_numbers) {
-	if (string_numbers === '') {
+	if (!Array.isArray(string_numbers) || string_numbers.length === 0) {
 		return 0;
 	}
 
-	if (matchesNumbers(string_numbers, [1, 2, 3, 4, 5])) {
-		return 6;
+	if (string_numbers.some(function(number) {
+		return number < 1 || number > 6;
+	})) {
+		return 'invalid dice value';
 	}
 
-	if (matchesNumbers(string_numbers, [2, 2, 3, 3])) {
-		return 4;
-	}
+	return string_numbers.reduce(function(total, number) {
+		if (number % 2 === 1) {
+			return total + (number - 1);
+		}
 
-	if (matchesNumbers(string_numbers, [6, 6, 4, 4, 1, 3])) {
-		return 2;
-	}
-
-	if (matchesNumbers(string_numbers, [3, 5, 3, 5, 4, 2])) {
-		return 12;
-	}
+		return total;
+	}, 0);
 };
