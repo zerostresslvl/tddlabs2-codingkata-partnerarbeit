0c651f903edbea8ed7d707516bbcb4fd60164640
Muhittin <muhittinsungur40@gmail.com>
2026-05-25 15:43:58 +0200

    055 finished writing failure part for partner

--- string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
+++ string calculator-20260525/string_calculator/spec/Electrons_around_the_cores_spec.js
@@ -8,10 +8,14 @@ describe('Electrons around the cores', function() {
 	});
 
 	it('should return the correct result for [1, 2, 3, 4, 5]', function() {
-	expect(calculator.add([1, 2, 3, 4, 5])).toEqual(6);
-});
+		expect(calculator.add([1, 2, 3, 4, 5])).toEqual(6);
+	});
 
 	it('should return the correct result for [2, 2, 3, 3]', function() {
 		expect(calculator.add([2, 2, 3, 3])).toEqual(4);
+	});
+
+	it('should return the correct result for [6, 6, 4, 1, 3]', function() {
+		expect(calculator.add([6, 6, 4, 1, 3])).toEqual(2);
 	});
 });
