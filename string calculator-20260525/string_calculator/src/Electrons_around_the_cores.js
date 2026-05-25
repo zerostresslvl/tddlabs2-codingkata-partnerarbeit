b3480f5fafb6448a4edd242f05e8dfe513133a7b
Muhittin <muhittinsungur40@gmail.com>
2026-05-25 15:37:02 +0200

    055 production code writen for failure test

--- string calculator-20260525/string_calculator/src/Electrons_around_the_cores.js
+++ string calculator-20260525/string_calculator/src/Electrons_around_the_cores.js
@@ -8,4 +8,8 @@ StringCalculator.prototype.add = function(string_numbers) {
 	if (string_numbers.toString() === [1, 2, 3, 4, 5].toString()) {
 		return 6;
 	}
+
+	if (string_numbers.toString() === [2, 2, 3, 3].toString()) {
+		return 4;
+	}
 };
